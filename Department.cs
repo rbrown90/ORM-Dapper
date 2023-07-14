efec5e159bff176a8c9cf2547de2c48a9171078c
Rakin Brown <rakinbrown38@gmail.com>
2023-07-14 17:12:42 -0500

    dapper exercise 1

---
+++ Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ORM_Dapper
+{
+	public class Department
+	{
+		public Department()
+		{
+		}
+
+		public int DepartmentID { get; set; }
+		public string Name { get; set; }
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+        }
+    }
+}
